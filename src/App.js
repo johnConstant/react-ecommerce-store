6b8065ce5164006e3e07bb6178010bbe423233e7
John Constant <johnconstantis@gmail.com>
2022-03-03 23:14:53 +0000

    Nav, Footer and Sidebar Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,49 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Navbar, Sidebar, Footer } from './components'
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Navbar, Sidebar, Footer } from './components';
+import {
+    About,
+    Cart,
+    Checkout,
+    Error,
+    Home,
+    PrivateRoute,
+    Products,
+    SingleProduct,
+} from './pages';
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+    return (
+        <Router>
+            <Navbar />
+            <Sidebar />
+            <Switch>
+                <Route path="/" exact>
+                    <Home />
+                </Route>
+                <Route path="/about" exact>
+                    <About />
+                </Route>
+                <Route path="/products/" exact>
+                    <Products />
+                </Route>
+                <Route path="/products/:id" children={<SingleProduct />} />
+                <Route path="/cart">
+                    <Cart />
+                </Route>
+                <Route path="/checkout" exact>
+                    <Checkout />
+                </Route>
+                {/* <Route path="/private-route" exact>
+                    <PrivateRoute />
+                </Route> */}
+                <Route path="*">
+                    <Error />
+                </Route>
+            </Switch>
+            <Footer />
+        </Router>
+    );
 }
 
-export default App
+export default App;
