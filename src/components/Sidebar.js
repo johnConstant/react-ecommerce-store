5157ef1c8d4969b8d377a5b7d6a20616f0290a33
John Constant <johnconstantis@gmail.com>
2022-03-14 21:55:29 +0000

    Sidebar functionality & Products Context

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -9,13 +9,22 @@ import CartButtons from './CartButtons';
 import { useUserContext } from '../context/user_context';
 
 const Sidebar = () => {
-    const isOpen = true;
+    const { isSidebarOpen, closeSidebar } = useProductsContext();
+
     return (
         <SidebarContainer>
-            <aside className={`${isOpen ? 'sidebar show-sidebar' : 'sidebar'}`}>
+            <aside
+                className={`${
+                    isSidebarOpen ? 'sidebar show-sidebar' : 'sidebar'
+                }`}
+            >
                 <div className="sidebar-header">
                     <img src={logo} alt="Comfy Sloth" className="logo" />
-                    <button type="button" className="close-btn">
+                    <button
+                        type="button"
+                        className="close-btn"
+                        onClick={closeSidebar}
+                    >
                         <FaTimes />
                     </button>
                 </div>
@@ -24,12 +33,16 @@ const Sidebar = () => {
                         const { id, text, url } = link;
                         return (
                             <li key={id}>
-                                <Link to={url}>{text}</Link>
+                                <Link to={url} onClick={closeSidebar}>
+                                    {text}
+                                </Link>
                             </li>
                         );
                     })}
                     <li>
-                        <Link to="/checkout">Checkout</Link>
+                        <Link to="/checkout" onClick={closeSidebar}>
+                            Checkout
+                        </Link>
                     </li>
                 </ul>
                 <CartButtons />
